1dbffa3b3edd55dca1202ce492597f48c1041638
deepblue <deepblue@inha.ac.kr>
2025-02-18 10:08:30 +0900

    v1.8 day07 stack debugger

--- day07.py
+++ day07.py
@@ -1,30 +1,18 @@
-class Stack:
-    def __init__(self):
-        self.items = list()
-
-    def push(self, data) -> object:
-        self.items.append(data)
-
-    def pop(self) -> object:
-        return self.items.pop()
-
-    def size(self) -> int:
-        return len(self.items)
-
-    def peek(self) -> object:
-        return self.items[-1]
-
-    def is_empty(self) -> bool:
-        return len(self.items) == 0
+def a(i):
+    j = 9
+    print(i, j)
 
 
-s1 = Stack()
-s1.push(-9)
-s1.push(11)
-s1.push(977)
-print(s1.pop())
-print(s1.peek())
-print(s1.peek())
-# print(s1.pop())
-# print(s1.pop())
-print(s1.is_empty())
+def b(k):
+    a(1)
+    print(k)
+
+
+def main():
+    print("start")
+    b(3)
+    print("end")
+
+
+if __name__ == "__main__":
+    main()
