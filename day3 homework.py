a15e09b5dde5498508238acd10a1689b6d87a68b
dissolve <dissolve1882@naver.com>
2025-02-19 16:23:51 +0900

    v3.5 day3 homework

--- day3 homework.py
+++ day3 homework.py
@@ -1,2 +1,102 @@
-# 이진 탐색 트리(BST) 삽입, 검색, 삭제 함수를 설계하고 코드로 작성하시오.
-# 삭제 함수에는 리프(LEAF) 노드, 자식이 1개인 경우 그리고 자식이 2개인 경우를 고려하시오.
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        new_node = TreeNode(data)
+        if self.root is None:
+            self.root = new_node
+            return
+        current = self.root
+        while True:
+            if data < current.data:
+                if current.left is None:
+                    current.left = new_node
+                    return
+                current = current.left
+            else:
+                if current.right is None:
+                    current.right = new_node
+                    return
+                current = current.right
+
+    def search(self, data):
+        current = self.root
+        while current:
+            if data == current.data:
+                return True
+            elif data < current.data:
+                current = current.left
+            else:
+                current = current.right
+        return False
+
+    def delete(self, data):
+        self.root = self._delete_node(self.root, data)
+
+    def _delete_node(self, node, data):
+        if node is None:
+            return node
+
+        if data < node.data:
+            node.left = self._delete_node(node.left, data)
+        elif data > node.data:
+            node.right = self._delete_node(node.right, data)
+        else:
+            # Case 1: Leaf node
+            if node.left is None and node.right is None:
+                return None
+            # Case 2: One child
+            elif node.left is None:
+                return node.right
+            elif node.right is None:
+                return node.left
+            # Case 3: Two children
+            else:
+                min_larger_node = self._find_min(node.right)
+                node.data = min_larger_node.data
+                node.right = self._delete_node(node.right, min_larger_node.data)
+        return node
+
+    def _find_min(self, node):
+        while node.left:
+            node = node.left
+        return node
+
+    def inorder_traversal(self, node):
+        if node:
+            self.inorder_traversal(node.left)
+            print(node.data, end=' ')
+            self.inorder_traversal(node.right)
+
+
+# 실행 코드
+if __name__ == "__main__":
+    bst = BST()
+    numbers = [10, 15, 8, 3, 9]
+    for num in numbers:
+        bst.insert(num)
+
+    print("BST 구성 완료")
+    print("중위 순회 결과:", end=' ')
+    bst.inorder_traversal(bst.root)
+    print()
+
+    find_group = int(input("검색할 번호를 입력하세요: "))
+    if bst.search(find_group):
+        print(f"{find_group}을(를) 찾았습니다")
+    else:
+        print(f"{find_group}이(가) 존재하지 않습니다")
+
+    delete_num = int(input("삭제할 번호를 입력하세요: "))
+    bst.delete(delete_num)
+    print("삭제 후 중위 순회 결과:", end=' ')
+    bst.inorder_traversal(bst.root)
+    print()
