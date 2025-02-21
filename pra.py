9cb29eb007192798a6de0a9ef87695de72b5434c
dissolve <dissolve1882@naver.com>
2025-02-21 10:22:08 +0900

    v5.0 day10 bubble_sort

--- pra.py
+++ pra.py
@@ -1,20 +1,5 @@
-from collections import deque
+from bisect import bisect_left
 
-class TreeNode:
-	def __init__(self):
-		self.left = None
-		self.data = None
-		self.right = None
-
-root = TreeNode()
-root.data = 10
-root.left = TreeNode()
-root.left.data = 5
-root.right = TreeNode()
-root.right.data = 15
-
-queue = deque([root])
-current = queue.popleft()  # 루트 노드(10)를 가져옴
-print(current.data)  # 10
-
+sorted_fruits = ['apple', 'banana', 'orange', 'plum']
+bisect_left(sorted_fruits, 'banana')
 
